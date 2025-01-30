101ce3665ab2714884bf722b3293a2a6c3618fbd
Saimanoj Napa <saimanoj.napa@appian.com>
2025-01-30 23:51:36 +0530

    formatted the code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,74 +1,92 @@
-import React from 'react';
-import {Container,Grid,Box,Button, Typography} from '@material-ui/core'
-import { Link } from 'react-router-dom';
-import useStyles from './styles';
+import React from "react";
+import { Grid, Box, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import useStyles from "./styles";
 
 const Footer = () => {
-    const classes = useStyles();
-    return (
-        <>
-        <Box color="text.primary" clone>
-        <Grid container className={classes.container} spacing={3} >
-         
-        <Grid item xs={12} sm={6} className={classes.box1}>
-
-            <Box >
-               
-                 <Typography variant="h5" color='textPrimary'>About us </Typography>
-                 <Typography variant="h7"  color='textSecondary'>
-                      A paragraph is a series of related sentences developing a central idea, called the topic. Try to think about paragraphs in terms of thematic unity: a paragraph is a sentence or a group of sentences that supports one central, unified idea. Paragraphs add one idea at a time to your broader argument.
-                </Typography>
+  const classes = useStyles();
+  return (
+    <>
+      <Box color="text.primary" clone>
+        <Grid container className={classes.container} spacing={3}>
+          <Grid item xs={12} sm={6} className={classes.box1}>
+            <Box>
+              <Typography variant="h5" color="textPrimary">
+                About us{" "}
+              </Typography>
+              <Typography variant="h7" color="textSecondary">
+                A paragraph is a series of related sentences developing a
+                central idea, called the topic. Try to think about paragraphs in
+                terms of thematic unity: a paragraph is a sentence or a group of
+                sentences that supports one central, unified idea. Paragraphs
+                add one idea at a time to your broader argument.
+              </Typography>
             </Box>
-            </Grid>
-            <Grid item xs={6} sm={3} className={classes.box2}>
-               <Box >
-               <Link to="/"  style={{ textDecoration: 'none' }} color='inherit' >
-                 <Typography variant="h5" gutterBottom color='textPrimary'>Help</Typography>
-               </Link>
-               </Box>
-               <Box>
-               <Link to="/" style={{ textDecoration: 'none' }}>
-                 <Typography variant="h7"  color='textSecondary'>Contact </Typography>
-               </Link>
-               </Box>
-               <Box >
-               <Link to="/" style={{ textDecoration: 'none' }} >
-                 <Typography variant="h7" color='textSecondary'>Privacy </Typography>
-               </Link>
-               </Box>
-               <Box >
-               <Link to="/" style={{ textDecoration: 'none' }} >
-                 <Typography variant="h7" color='textSecondary'>Support </Typography>
-               </Link>
-               </Box>
-            </Grid>
-            <Grid item xs={6} sm={3} className={classes.box3}>
-            <Box >
-               <Link to="/"  style={{ textDecoration: 'none' }} color='inherit' >
-                 <Typography variant="h5" gutterBottom color='textPrimary'>Help</Typography>
-               </Link>
-               </Box>
-               <Box>
-               <Link to="/" style={{ textDecoration: 'none' }}>
-                 <Typography variant="h7"  color='textSecondary'>Contact </Typography>
-               </Link>
-               </Box>
-               <Box >
-               <Link to="/" style={{ textDecoration: 'none' }} >
-                 <Typography variant="h7" color='textSecondary'>Privacy </Typography>
-               </Link>
-               </Box>
-               <Box >
-               <Link to="/" style={{ textDecoration: 'none' }} >
-                 <Typography variant="h7" color='textSecondary'>Support </Typography>
-               </Link>
-               </Box>
-            </Grid>
-           
+          </Grid>
+          <Grid item xs={6} sm={3} className={classes.box2}>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }} color="inherit">
+                <Typography variant="h5" gutterBottom color="textPrimary">
+                  Help
+                </Typography>
+              </Link>
+            </Box>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Typography variant="h7" color="textSecondary">
+                  Contact{" "}
+                </Typography>
+              </Link>
+            </Box>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Typography variant="h7" color="textSecondary">
+                  Privacy{" "}
+                </Typography>
+              </Link>
+            </Box>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Typography variant="h7" color="textSecondary">
+                  Support{" "}
+                </Typography>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={6} sm={3} className={classes.box3}>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }} color="inherit">
+                <Typography variant="h5" gutterBottom color="textPrimary">
+                  Help
+                </Typography>
+              </Link>
+            </Box>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Typography variant="h7" color="textSecondary">
+                  Contact{" "}
+                </Typography>
+              </Link>
+            </Box>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Typography variant="h7" color="textSecondary">
+                  Privacy{" "}
+                </Typography>
+              </Link>
+            </Box>
+            <Box>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Typography variant="h7" color="textSecondary">
+                  Support{" "}
+                </Typography>
+              </Link>
+            </Box>
+          </Grid>
         </Grid>
-        </Box>
-        </>
-    )
-}
+      </Box>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,46 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
-import logo from '../../assests/vpc.PNG';
-import useStyles from './styles';
+import logo from "../../assests/vpc.PNG";
+import useStyles from "./styles";
 
-const NavBar = ({totalItems}) => {
-    const classes = useStyles();
-    return (
-        <div>
-             <AppBar position="fixed" className={classes.appBar} color="inherit">
+const NavBar = ({ totalItems }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt="Online Rice Store Nellore" height="25px" className={classes.image} />
-             Online Rice Store Nellore
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Online Rice Store Nellore"
+              height="25px"
+              className={classes.image}
+            />
+            Online Rice Store Nellore
           </Typography>
           <div className={classes.grow} />
           <div className={classes.button}>
-            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+            <IconButton
+              component={Link}
+              to="/cart"
+              aria-label="Show cart items"
+              color="inherit"
+            >
               <Badge badgeContent={totalItems} color="secondary">
                 <ShoppingCart />
               </Badge>
@@ -26,8 +48,8 @@ const NavBar = ({totalItems}) => {
           </div>
         </Toolbar>
       </AppBar>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default NavBar;
