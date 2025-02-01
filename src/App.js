01142041fb68d12b4727408caac6e9e37f337daf
Saimanoj Napa <saimanoj.napa@appian.com>
2025-02-01 20:44:24 +0530

    fixed cart aligned issue

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React, { useState, useEffect } from "react";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import {
-  Navbar,
-  Products,
-  Cart,
-  CheckOut,
-  ReactHookForm,
-  Footer,
-} from "./components";
+import { Navbar, Products, Cart, CheckOut, Footer } from "./components";
 import res from "./api/items.json";
 import theme from "./materialui/theme";
 
@@ -18,16 +11,7 @@ const App = () => {
   const [cart, setCart] = useState({
     noOfitems: 0,
     totalAmount: 0,
-    items: [
-      res.items[0],
-      res.items[1],
-      res.items[2],
-      res.items[3],
-      res.items[4],
-      res.items[5],
-      res.items[6],
-      res.items[7],
-    ],
+    items: [],
   });
 
   const fetchProducts = async () => {
@@ -39,14 +23,19 @@ const App = () => {
     setCart((prevState) => ({
       noOfitems: prevState.noOfitems + quantity,
       totalAmount: prevState.totalAmount + product.price,
-      //items : [...prevState.items,product]
-      items: cart.items.map((item) => {
-        if (item.id === product.id) {
-          item.quantity = item.quantity + quantity;
-          item.subTotal = item.subTotal + item.price;
-        }
-        return item;
-      }),
+      // items: [...prevState.items, product],
+
+      items: prevState.items.find((item) => {
+        return item.id === product.id;
+      })
+        ? cart.items.map((item) => {
+            if (item.id === product.id) {
+              item.quantity = item.quantity + quantity;
+              item.subTotal = item.subTotal + item.price;
+            }
+            return item;
+          })
+        : [...prevState.items, product],
     }));
     console.log(cart);
   };
@@ -125,9 +114,6 @@ const App = () => {
             </Route>
             <Route path="/checkout" exact>
               <CheckOut cart={cart} />
-            </Route>
-            <Route path="/form" exact>
-              <ReactHookForm />
             </Route>
           </Switch>
         </div>
--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -1,37 +1,65 @@
-import React from 'react';
-import { Container, Typography, Button, Grid } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-import CartItem from './CartItem/CartItem';
-import useStyles from './styles';
+import CartItem from "./CartItem/CartItem";
+import useStyles from "./styles";
 
-const Cart = ({ cart, onUpdateCartQty,removeItem,makeCartEmpty }) => {
+const Cart = ({ cart, onUpdateCartQty, removeItem, makeCartEmpty }) => {
   const classes = useStyles();
 
-
   const renderEmptyCart = () => (
-    <Typography variant="subtitle1">You have no items in your shopping cart,
-      <Link className={classes.link} to="/">start adding some</Link>!
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart,
+      <Link className={classes.link} to="/">
+        start adding some
+      </Link>
+      !
     </Typography>
   );
 
-  if (!cart.items) return 'Loading';
+  if (!cart.items) return "Loading";
 
   const renderCart = () => (
     <>
       <Grid container spacing={3}>
-        {cart.items.map((lineItem) => (
-          <Grid item xs={12} sm={4} key={lineItem.id}>
-            <CartItem item={lineItem} onUpdateCartQty={onUpdateCartQty} removeItem={removeItem} />
-          </Grid>
-        ))}
+        {cart.items.map(
+          (lineItem) =>
+            lineItem.quantity !== 0 && (
+              <Grid item xs={12} sm={4} key={lineItem.id + Math.random()}>
+                <CartItem
+                  item={lineItem}
+                  onUpdateCartQty={onUpdateCartQty}
+                  removeItem={removeItem}
+                />
+              </Grid>
+            )
+        )}
       </Grid>
       <div className={classes.cardDetails}>
         <Typography variant="h4">Subtotal: {cart.totalAmount}</Typography>
         <div>
-          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" 
-           onClick={()=>makeCartEmpty()}>Empty cart</Button>
-          <Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" color="primary">Checkout</Button>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+            onClick={() => makeCartEmpty()}
+          >
+            Empty cart
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            component={Link}
+            to="/checkout"
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
         </div>
       </div>
     </>
@@ -40,8 +68,10 @@ const Cart = ({ cart, onUpdateCartQty,removeItem,makeCartEmpty }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
-      { !cart.noOfitems ? renderEmptyCart() : renderCart() }
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {cart.noOfitems === 0 ? renderEmptyCart() : renderCart()}
     </Container>
   );
 };
